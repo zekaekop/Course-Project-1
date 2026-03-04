a2995553a98bbcd39aafad7fcc4bc3612a4041f1
zekaeko.p <zekaeko.p@gmail.com>
2026-03-05 00:06:25 +0300

    login.php no longer uses base.php bc of header and fixes POST logic account login and adds error handling

--- login.php
+++ login.php
@@ -1,6 +1,10 @@
 <?php
+
+
 session_start();
 include "db.php";
+
+$error = '';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
@@ -8,29 +12,53 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     $email = trim($_POST["email"]);
     $password = trim($_POST["password"]);
 
-    $query =  $pdo->prepare ("SELECT * FROM course_project.users where name = ?;");
-    $query -> execute([$name]);
+    $query =  $pdo->prepare ("SELECT * FROM course_project.Users where name = ? OR mail = ?;");
+    $query -> execute([$name, $email]);
     $user = $query-> fetch();
 
-    if($name == $user['name'] && $password == $user['password']){ 
+    if($name == $user['name'] && 
+        $password == $user['password'] &&
+        $email == $user['mail']){ 
+
         $_SESSION['user'] = $user;
-        header("location: index.php");
+        header("location: home.php");
+    }
+    else{
+        $error = "Something went wrong :p";
     }
 
     }
-
-include "base.php";
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Course Project</title>
+   <link rel="stylesheet" href="static/css/bootstrap.min.css">
+    <link rel="stylesheet" href="static/css/base.css">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
 
 <div class="mt-5">
     <h1 class="text-center">Login</h1>
     <h5  class="text-center">into an existing account</h5>
 
     <div class="d-flex justify-content-center">
-        <form action="post">
+        <form method="POST" action="">
             <div class="card">
                 
+                <?php if ($error): ?>         <!--  display login error if something goes wrong -->
+                    <div class="alert alert-danger"><?php echo htmlspecialchars($error); ?></div>
+                <?php endif; ?> <!-- apperantly php ignores html comments -->
+                
+
                 <ul class="p-0">
                     <li><h4>Username</h4><input type="text" name="name" id=""></li>
                     <li><h4>Email</h4><input type="text" name="email" id=""></li>
