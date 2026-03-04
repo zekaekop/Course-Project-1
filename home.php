0798eb42e615bc1b6781ab8a0ba8f5fac4a8c0b4
zekaeko.p <zekaeko.p@gmail.com>
2026-03-05 00:11:20 +0300

    adding the basic idea layout of the home page?

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+
 include "db.php";
 
 include "base.php";
@@ -8,29 +8,24 @@ include "base.php";
 
 <div class="mt-5">
     <h1 class="text-center">Home Page</h1>
-    <h5  class="text-center">into an existing account</h5>
 
     <div class="d-flex justify-content-center">
         <form action="post">
+
             <div class="card">
-                
-                <ul class="p-0">
-                    <li><h4>Username</h4><input type="text" name="name" id=""></li>
-                    <li><h4>Email</h4><input type="text" name="email" id=""></li>
-                    <li><h4>Password</h4><input type="password" name="password" id=""></li>
-                </ul>
-
-                <button class="btn btn-primary w-100" type="submit">
-                    Enter
-                </button>
-
-                <div class="d-flex justify-content-end">
-                    <a href="/register.php">
-                        <small  class="text-center">Don't have an account?</small>
-                    </a>
-                </div>
-
+                welcome bla bla
             </div>
+
+            <br>
+
+            <div class="card">
+                our best courses
+            </div>
+
+            <div class="card">
+                create a course (teacher, admin only!)
+            </div>
+
         </form>
     </div>
 </div>
