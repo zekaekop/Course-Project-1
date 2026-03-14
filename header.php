4ba00e42bd5dec5b951a30b4d0051cbabd779512
zekaeko.p <zekaeko.p@gmail.com>
2026-03-14 15:34:09 +0300

    pages now use container at limited width to get a formlike feel and also added animations on the header and enhanced a bit of the home page

--- header.php
+++ header.php
@@ -4,34 +4,37 @@ include "db.php";
 
 ?>
 
-<div class="nav navbar ">
-    <div class="d-flex justify-content-center w-100">
+<div class="container">
+    <div class="nav navbar pt-0">
+        <div class="w-100">
 
-        <form method="POST" action="">
-            <div class="card">
+            <form method="POST" action="">
+                <div class="card w-100">
 
-                <nav>
-                    <ul class="nav">
-                        <li class="nav-item"><a href="courses.php" class="nav-link text-selected">Courses</a></li>
-                        <li class="nav-item"><a href="categories.php" class="nav-link text-selected">Category</a></li>
-                        
-                        <?php if (True): ?>
-                            <li class="nav-item">
-                                <a href="admin.php" class="nav-link text-selected">Admin Panel</a>
+                    <nav class="d-flex">
+                        <ul class="nav w-100">
+                            <li class="nav-item"><a href="courses.php" class="nav-link text-selected">Courses</a></li>
+                            <li class="nav-item"><a href="categories.php" class="nav-link text-selected">Category</a></li>
+                            
+                            <?php if (True): ?>
+                                <li class="nav-item">
+                                    <a href="admin.php" class="nav-link text-selected">Admin Panel</a>
+                                </li>
+                            <?php endif; ?>
+
+                            <li class="nav-item mt-lg-0 ms-lg-auto">
+                                <a href="logout.php" class="nav-link text-selected w-100 w-lg-auto">
+                                    Logout
+                                </a>
                             </li>
-                        <?php endif; ?>
+                            
+                        </ul>
+                    </nav>
 
-                        <li class="nav-item"><a href="logout.php" class="nav-link text-selected">Logout</a></li>
+                </div>
+            </form>
 
-                    </ul>
-                </nav>
-
-            </div>
-        </form>
-
+        </div>
     </div>
 </div>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
--- home.php
+++ home.php
@@ -4,16 +4,19 @@ include "db.php";
 
 include "base.php";
 
+$user = $_SESSION['user'];
+
 ?>
 
-<div class="mt-5">
+<div class="container mt-5">
     <h1 class="text-center">Home Page</h1>
 
     <div class="d-flex justify-content-center">
         <form action="post">
 
-            <div class="card">
-                welcome bla bla
+            <div class="card w-100 text-center">
+                <h3>Welcome <?= $user['name'] ?></h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ex pariatur iure quas tenetur accusantium est laudantium id expedita, autem ea porro quos officiis deleniti, cumque error neque exercitationem praesentium?</p>
             </div>
 
             <br>
@@ -33,5 +36,3 @@ include "base.php";
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
