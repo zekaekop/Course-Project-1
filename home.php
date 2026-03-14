cbcec91a0ea745c3652afc2e2ea1d3984174ff7c
zekaeko.p <zekaeko.p@gmail.com>
2026-03-14 21:07:42 +0300

    fixed indent on home

--- home.php
+++ home.php
@@ -10,36 +10,32 @@ $user = $_SESSION['user'];
 
 <div class="container mt-5">
     <h1 class="text-center">Home Page</h1>
+    <div class="card w-100 mt-3 text-center">
+        <h3>Welcome <?= $user['name'] ?></h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ex pariatur iure quas tenetur accusantium est laudantium id expedita, autem ea porro quos officiis deleniti, cumque error neque exercitationem praesentium?</p>
+    </div>
 
-    <div class="d-flex justify-content-center">
+    <br>
 
-        <div class="card w-100 mt-3 text-center">
-            <h3>Welcome <?= $user['name'] ?></h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ex pariatur iure quas tenetur accusantium est laudantium id expedita, autem ea porro quos officiis deleniti, cumque error neque exercitationem praesentium?</p>
-        </div>
+    <div class="card w-100 mt-3">
+        <h3 class="text-center">Enlisted Courses</h3>
 
-        <br>
+        <table>
+            <thead>
+                <tr>
+                    <th>Course Name</th>
+                    <th>Course Description</th>
+                    <th>User</th>
+                    <th>Assigned Date</th>
+                </tr>
+            </thead>
+        <tr>
+            <td>bla course</td>
+            <td>eko</td>
+            <td>today</td>
+        </tr>
+        </table>
 
-        <div class="card w-100 mt-3">
-            <h3 class="text-center">Enlisted Courses</h3>
-
-            <table>
-                <thead>
-                    <tr>
-                        <th>Course</th>
-                        <th>User</th>
-                        <th>Assigned Date</th>
-                    </tr>
-                </thead>
-            <tr>
-                <td>bla course</td>
-                <td>eko</td>
-                <td>today</td>
-            </tr>
-            </table>
-
-        </div>
-        
     </div>
 </div>
 
