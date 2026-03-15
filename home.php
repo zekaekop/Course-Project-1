999695389e1c765b618e18092c32691102e9a9ac
zekaeko.p <zekaeko.p@gmail.com>
2026-03-15 15:41:58 +0300

    the home page now only shows the users enlisted courses and now everyone elses

--- home.php
+++ home.php
@@ -6,14 +6,18 @@ include "base.php";
 
 $user = $_SESSION['user'];
 $search = '';
+
+$user_id = $user['user_id'];
+
 if (isset($_GET[$search])){ # Search list
-    $query = $pdo -> prepare("SELECT * FROM course_project.account_data WHERE name LIKE ? OR course_name LIKE ?");
+    $query = $pdo -> prepare("SELECT * FROM course_project.account_data WHERE name LIKE ? OR course_name LIKE ? OR name LIKE $username ");
     $query -> execute("%$search%","%$search%");
     $account_data = $query -> fetchAll();
 } else { # Regular list
     $query = $pdo -> query("SELECT * FROM course_project.account_data ad 
                                             JOIN course_project.courses c ON ad.course_id = c.course_id # apperantly i have to call user_id not user_name
-                                            JOIN course_project.users u ON ad.user_id = u.user_id ");  # after that i can just call any column in that table
+                                            JOIN course_project.users u ON ad.user_id = u.user_id  # after that i can just call any column in that table
+                                            WHERE ad.user_id LIKE $user_id ");  
     
     $account_data = $query -> fetchAll();
 }
